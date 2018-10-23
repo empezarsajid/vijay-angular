de0a54b2869d224a522643f64a312611cb6d8416
Sajid <empezar.sajid@gmail.com>
2018-10-23 19:18:56 +0530

    Loggers added

--- AngularJs/Program.cs
+++ AngularJs/Program.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
+using Serilog;
 namespace AngularJs
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            var gcpCredentaialPath = "client_secret.json";
+            System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", gcpCredentaialPath);
             BuildWebHost(args).Run();
         }
 
